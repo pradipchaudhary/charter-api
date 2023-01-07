73dd148ebb123f659497c4b6da3eccbd1de4503a
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-01-07 21:08:20 +0545

    Server Created!

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const app = express();
+import express from "express";
+import bodyParser from "body-parser";
 
-const port = 5000;
-app.listen(port, ()=>{
-    console.log("Server Start!")
-})
+// Application
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
